d71c48245193db8f27cade9ec216581788aff380
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-04-02 14:48:15 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Hotel Booking Management System Main Entry Point");
+    }
+}
+
+
+//UC1: Application Entry & Welcome Message
+class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
