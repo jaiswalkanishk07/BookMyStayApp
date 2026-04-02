77aa309cc30ee62932a062a8517ca2786c526073
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-04-02 14:59:50 +0530

    UC2: Room Initialization & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -12,3 +12,73 @@ class UseCase1HotelBookingApp {
         System.out.println("System initialized successfully.");
     }
 }
+
+
+//UC2: Room Initialization & Static Availability
+class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+        System.out.println("Hotel Room Initialization\n");
+
+        // Initializing specific room types with predefined attributes
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        // Static availability (represented using simple variables for now)
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Displaying details
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+
+    // --- Domain Models ---
+
+    abstract static class Room {
+        protected int numberOfBeds;
+        protected int squareFeet;
+        protected double pricePerNight;
+
+        public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+            this.numberOfBeds = numberOfBeds;
+            this.squareFeet = squareFeet;
+            this.pricePerNight = pricePerNight;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Beds: " + numberOfBeds);
+            System.out.println("Size: " + squareFeet + " sqft");
+            System.out.println("Price per night: " + pricePerNight);
+        }
+    }
+
+    static class SingleRoom extends Room {
+        public SingleRoom() {
+            super(1, 250, 1500.0);
+        }
+    }
+
+    static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super(2, 400, 2500.0);
+        }
+    }
+
+    static class SuiteRoom extends Room {
+        public SuiteRoom() {
+            super(3, 750, 5000.0);
+        }
+    }
+}
